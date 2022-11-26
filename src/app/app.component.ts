91b9fee61406aaccf3e261e034858b5f67dcff6a
AleksRo <67026178+AleksRo@users.noreply.github.com>
2022-11-26 12:02:42 +0200

    meta upd 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SeoService } from './services/seo.service';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'film-app';
+
+
+  constructor(private seo:SeoService) {
+
+  }
+
+
+  ngOnInit() {
+    this.seo.initDefaultMetaInfomation()
+  }
 }
 

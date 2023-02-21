3b42d87c08c2b65753148df3413070d8b90ba681
root <root@vm899181.stark-industries.solutions>
2023-02-21 19:38:01 +0100

    r

--- src/app/player/player.component.ts
+++ src/app/player/player.component.ts
@@ -36,21 +36,11 @@ export class PlayerComponent implements OnInit {
   ) { }
 
   ngOnInit() {
-    fetch('https://base.ashdi.vip/api/product/read_one.php?imdb=tt6341832&api_key=99a660-8378e4-4adb0a-eeeb92-86b677')
-      .then(response => response.json())
-      .then(data => {
-        this.data = data;
-      })
-      .catch(error => console.error(error));
-  }
-
-  ashvid() {
-    this.api.getAshvid(this.film.imdb_id).subscribe((data) => {
-      this.asvPlayer = data.url
-      console.log(this.asvPlayer);
-      
-      debugger
-    })
+    if(this.film.shiki_id) {
+      let shId = this.film.shiki_id
+      this.srcPlayer = this.sanitizer.bypassSecurityTrustResourceUrl("https://kodik.cc/find-player?shikimoriID="+shId);
+    }
+    this.srcPlayer = this.sanitizer.bypassSecurityTrustResourceUrl("https://12.svetacdn.in/vDqR81AxhrhI?imdb_id="+this.film.imdb_id);
   }
 
   onPlayer(player: string) {
@@ -64,8 +54,8 @@ export class PlayerComponent implements OnInit {
     if(player ==='cdn' && this.film.imdb_id) {
         this.srcPlayer = this.sanitizer.bypassSecurityTrustResourceUrl("https://12.svetacdn.in/vDqR81AxhrhI?imdb_id="+this.film.imdb_id);
     }
-     if(player ==='ASHDI' && this.film.imdb_id) {
+    /*  if(player ==='ASHDI' && this.film.imdb_id) {
       this.srcPlayer = this.sanitizer.bypassSecurityTrustResourceUrl(this.asvPlayer)
-    } 
+    }  */
   }
 }
